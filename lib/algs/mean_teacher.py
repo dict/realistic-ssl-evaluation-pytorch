e659d2e9d726f47c59135c20f503ed977f9a3b54
dict <dict@estsoft.com>
2019-12-31 06:01:13 +0000

    change with mean teacher

--- lib/algs/mean_teacher.py
+++ lib/algs/mean_teacher.py
@@ -10,6 +10,7 @@ class MT(nn.Module):
         self.ema_factor = ema_factor
         self.global_step = 0
 
+    """
     def forward(self, x, y, model, mask):
         self.global_step += 1
         y_hat = self.model(x)
@@ -17,6 +18,34 @@ class MT(nn.Module):
         y = model(x) # recompute y since y as input of forward function is detached
         model.update_batch_stats(True)
         return (F.mse_loss(y.softmax(1), y_hat.softmax(1).detach(), reduction="none").mean(1) * mask).mean()
+    """
+
+    def forward(self, x, y, model):
+        self.global_step += 1
+        y_hat = self.model(x)
+        model.update_batch_stats(False)
+        y = model(x) # recompute y since y as input of forward function is detached
+        model.update_batch_stats(True)
+        
+        beta_coeff = 1
+        alphas = torch.exp(y_hat)
+        betas = torch.exp(y)
+        a_zero = torch.sum(alphas, -1)
+        
+        """
+        loss1 = torch.lgamma(a_zero) - torch.sum(torch.lgamma(alphas), -1)
+        loss2 = torch.sum(
+                    (alphas - betas) * (torch.digamma(alphas) - torch.digamma(a_zero.unsqueeze(-1))), -1)
+
+        kl_loss = loss1 + loss2
+        """
+        kl_loss = torch.lgamma(a_zero) - torch.lgamma(torch.sum(betas,-1)) - torch.sum(torch.lgamma(alphas),-1) + \
+                    torch.sum(torch.lgamma(betas),-1) + torch.sum((alphas-betas)*(torch.digamma(alphas) - torch.digamma(a_zero.unsqueeze(-1))),-1)
+        
+        
+        return kl_loss.mean()
+        #return (kl_loss * mask).mean()
+
 
     def moving_average(self, parameters):
         ema_factor = min(1 - 1 / (self.global_step+1), self.ema_factor)
